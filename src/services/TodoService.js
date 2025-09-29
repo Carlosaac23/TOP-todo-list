556b44ced9be442fe82a9f9d2b4cc84dccac58fb
Carlos <carlosaac232001@gmail.com>
2025-09-29 10:52:39 -0500

    feat: add localStorage to all functions

--- src/services/TodoService.js
+++ src/services/TodoService.js
@@ -1,30 +1,15 @@
-export const tasks = [
-  {
-    title: 'Titulo 1',
-    description: 'Soy una descripcion',
-    dueDate: '31/10/2025',
-    priority: 'medium',
-    checked: false,
-  },
-  {
-    title: 'Titulo 2',
-    description: 'Soy una descripcion 2',
-    dueDate: '31/12/2025',
-    priority: 'low',
-    checked: true,
-  },
-];
-
-class Task {
-  constructor(title, description, dueDate, priority, checked) {
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.checked = checked;
-  }
-}
+export let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 export function addTask(task) {
   tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+export function getTasks() {
+  return [...tasks];
+}
+
+export function deleteTask(id) {
+  tasks.filter(task => task.id !== id);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
