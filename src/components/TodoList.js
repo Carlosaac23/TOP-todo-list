46eb6e761b1fa982967e242a2527b363580a8ca9
Carlos <carlosaac232001@gmail.com>
2025-09-29 10:51:45 -0500

    feat: import getTasks function with new feat with localStorage and render them

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,13 @@
 import TodoItem from './TodoItem';
-import { tasks } from '../services/TodoService';
+import { getTasks } from '../services/TodoService';
 
 export default function TodoList() {
-  const todoListContainer = document.getElementById('todo-list');
+  const todoListContainer = document.getElementById('tasks-list');
   const todoList = document.createElement('ul');
+  todoList.classList.add('tasks-list');
+  todoListContainer.innerHTML = '';
+  const tasks = getTasks();
+
   console.log(tasks);
   tasks.forEach(task => {
     const { title, description, dueDate, priority, checked } = task;
