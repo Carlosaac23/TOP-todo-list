8c9dd3a315761534288e566473c620f375bc9ff6
Carlos <carlosaac232001@gmail.com>
2025-09-29 10:52:06 -0500

    feat: create models folder to insert Task model

---
+++ src/models/Task.js
@@ -0,0 +1,10 @@
+export default class Task {
+  constructor(id, title, description, dueDate, priority, checked) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.checked = checked;
+  }
+}
