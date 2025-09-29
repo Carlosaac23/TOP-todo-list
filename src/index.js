f6ae1e524f4850d15bf7fa0b84b741d1e398f447
Carlos <carlosaac232001@gmail.com>
2025-09-29 09:17:58 -0500

    feat: config main js file to test every component

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+import TodoList from './components/TodoList';
+import Footer from './components/Footer';
+import './style.css';
+
+function main() {
+  Header();
+  TodoList();
+  Footer();
+}
+
+main();
