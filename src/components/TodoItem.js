577d1a2a633fd54a436fc26b27f94329a9b594fb
Carlos <carlosaac232001@gmail.com>
2025-09-29 10:51:10 -0500

    feat: add semantic HMTL into TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,8 +1,15 @@
 export default function TodoItem(...props) {
   const [title, description, dueDate, priority, checked] = props;
 
-  const card = document.createElement('div');
-  card.textContent = `${title} ${description}`;
+  const card = document.createElement('li');
+  card.classList.add('card');
+  card.innerHTML = `
+    <h2>${title}</h2>
+    <p>${description}</p>
+    <p>${dueDate}</p>
+    <p>${priority}</p>
+    
+  `;
 
   return card;
 }
