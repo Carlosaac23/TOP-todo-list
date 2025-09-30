7ae357b8a49ac60ae07929763c6b69fce1d2dab2
Carlos <carlosaac232001@gmail.com>
2025-09-30 09:13:34 -0500

    chore: import new components names

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Header from './components/Header';
 import TaskForm from './components/TaskForm';
-import TodoList from './components/TodoList';
+import TasksList from './components/TasksList';
 import Footer from './components/Footer';
 import './styles/style.css';
 import './styles/reset.css';
@@ -8,7 +8,7 @@ import './styles/reset.css';
 function main() {
   Header();
   TaskForm();
-  TodoList();
+  TasksList();
   Footer();
 }
 
