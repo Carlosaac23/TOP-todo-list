237268a2307b75fb20030573ca3e1f28c4a96e72
Carlos <carlosaac232001@gmail.com>
2025-09-30 14:31:51 -0500

    refactor: - remove ul element because made it ul from index.html to reduce code - render a No tasks message when there is not tasks to show

--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -2,20 +2,21 @@ import TaskItem from './TaskItem';
 import { getTasks } from '../services/TodoService';
 
 export default function TasksList() {
-  const todoListContainer = document.getElementById('tasks-list');
-  const todoList = document.createElement('ul');
-  todoList.classList.add('tasks-list');
-  todoListContainer.innerHTML = '';
+  const tasksContainer = document.getElementById('tasks-container');
+  tasksContainer.innerHTML = '';
   const tasks = getTasks();
 
   console.log(tasks);
+
+  if (tasks.length === 0) {
+    tasksContainer.textContent = 'There are no tasks.';
+  }
+
   tasks.forEach(task => {
     const { id, title, description, dueDate, priority, checked } = task;
 
     const item = TaskItem(id, title, description, dueDate, priority, checked);
 
-    todoList.appendChild(item);
+    tasksContainer.appendChild(item);
   });
-
-  todoListContainer.appendChild(todoList);
 }
