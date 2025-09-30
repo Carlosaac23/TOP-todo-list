584cf389f984071f304ac9f801c39191650eaebf
Carlos <carlosaac232001@gmail.com>
2025-09-30 09:05:00 -0500

    feat: create EditForm to edit a task into a dialog

---
+++ src/components/EditForm.js
@@ -0,0 +1,72 @@
+import TasksList from './TasksList';
+import { editTask } from '../services/TodoService';
+
+export default function EditForm(task) {
+  const { id, title, description, dueDate, priority, checked } = task;
+
+  const dialog = document.createElement('dialog');
+  dialog.innerHTML = `
+    <button id="close-dialog">Close</button>
+
+    <form id="form">
+      <label for="title">Title</label>
+      <input type="text" name="title" id="title" value="${title}" />
+
+      <label for="decription">Description</label>
+      <textarea
+        name="description"
+        id="description"
+      >${description}</textarea>
+
+      <label for="due-date">Due Date</label>
+      <input type="date" name="due-date" id="due-date" value="${dueDate}" />
+
+      <fieldset>
+        <legend>Priority</legend>
+
+        <input type="radio" name="priority" id="high" value="high" ${
+          priority === 'high' ? 'checked' : ''
+        } />
+        <label for="high">High</label>
+
+        <input type="radio" name="priority" id="medium" value="medium" ${
+          priority === 'medium' ? 'checked' : ''
+        } />
+        <label for="medium">Medium</label>
+
+        <input type="radio" name="priority" id="low" value="low" ${
+          priority === 'low' ? 'checked' : ''
+        } />
+        <label for="low">Low</label>
+      </fieldset>
+
+      <button>Update</button>
+    </form>
+  `;
+
+  document.body.appendChild(dialog);
+  dialog.showModal();
+
+  dialog.querySelector('#close-dialog').addEventListener('click', () => {
+    dialog.close();
+    dialog.remove();
+  });
+
+  dialog.querySelector('#form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    const updatedTask = {
+      id,
+      title: dialog.querySelector('#title').value,
+      description: dialog.querySelector('#description').value,
+      dueDate: dialog.querySelector('#due-date').value,
+      priority: dialog.querySelector('input[name="priority"]:checked').value,
+      checked,
+    };
+
+    editTask(updatedTask);
+    TasksList();
+    dialog.close();
+    dialog.remove();
+  });
+}
