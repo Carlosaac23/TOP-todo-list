7c4141986239957178e272b253b7989c3729395a
Carlos <carlosaac232001@gmail.com>
2025-10-01 09:28:33 -0500

    feat: - add ternary operator to set one or another class to priority span - import some icons to buttons - change name classes into TaskDialog - improve deleteBtn function. nested render toast and render list only when delete is true - add doneBtn too to check task from dialog as well

--- src/components/TaskDialog.js
+++ src/components/TaskDialog.js
@@ -1,41 +1,54 @@
+import { deleteTask, editTask, getTasks } from '../services/TodoService';
+import { formatDate, renderToast, capitalizeWord } from '../utils/domUtils';
 import TasksList from './TasksList';
 import EditForm from './EditForm';
-import { deleteTask } from '../services/TodoService';
-import { formatDate } from '../utils/domUtils';
+import editIcon from '../icons/edit.svg';
+import deleteIcon from '../icons/delete.svg';
+import doneIcon from '../icons/check.svg';
 
 export default function TaskInfo(task) {
+  console.log('Desde TaskDialog', task);
   const { id, title, description, dueDate, priority, checked } = task;
+  const priorityLevel =
+    priority === 'high'
+      ? 'card__priority-high'
+      : priority === 'medium'
+      ? 'card__priority-medium'
+      : 'card__priority-low';
 
   const dialog = document.createElement('dialog');
   dialog.classList.add('card__dialog');
   dialog.innerHTML = `
-    <button id="close-dialog">Close</button>
+    <button class="card__dialog-close">Close</button>
 
-    <h2>${title}</h2>
-    <p>${description}</p>
-    <p>${formatDate(dueDate)}</p>
-    <p>${priority}</p>
-    <input name="${id}" id="${id}" type="checkbox" ${checked ? 'checked' : ''}>
-    <label for="${id}">Completed</label>
+    <h2 class="card__dialog-title">${title}</h2>
+    <div class="card__dialog-description">${description}</div>
+    <p><strong>Due Date:</strong> ${formatDate(dueDate)}</p>
+    <p class="card-priority">
+      <strong>Priority:</strong>
+      <span class="${priorityLevel}">${capitalizeWord(priority)}</span>
+    </p>
 
     <div class="card__dialog-buttons">
-      <button class="dialog__button" id="edit">Edit</button>
-      <button class="dialog__button" id="delete">Delete</button>
+      <button class="dialog__button-edit" id="edit">
+        <span>Edit</span>
+        <img src="${editIcon}" alt="edit icon" />
+      </button>
+      <button class="dialog__button-delete" id="delete">
+        <span>Delete</span>
+        <img src="${deleteIcon}" alt="delete icon" />
+      </button>
+      <button class="dialog__button-done" id="done">
+        <span>Done</span>
+        <img src="${doneIcon}" alt="check icon" />
+      </button>
     </div>
   `;
 
   document.body.appendChild(dialog);
   dialog.showModal();
 
-  dialog.querySelector('#close-dialog').addEventListener('click', () => {
-    dialog.close();
-    dialog.remove();
-  });
-
-  const deleteBtn = dialog.querySelector('#delete');
-  deleteBtn.addEventListener('click', () => {
-    deleteTask(id);
-    TasksList();
+  dialog.querySelector('.card__dialog-close').addEventListener('click', () => {
     dialog.close();
     dialog.remove();
   });
@@ -44,6 +57,31 @@ export default function TaskInfo(task) {
   editBtn.addEventListener('click', () => {
     EditForm({ id, title, description, dueDate, priority, checked });
 
+    dialog.close();
+    dialog.remove();
+  });
+
+  const deleteBtn = dialog.querySelector('#delete');
+  deleteBtn.addEventListener('click', () => {
+    const deleted = deleteTask(id);
+
+    if (deleted) {
+      TasksList();
+      renderToast('Task deleted successfully', 'green');
+      dialog.close();
+      dialog.remove();
+    }
+  });
+
+  const doneBtn = dialog.querySelector('#done');
+  doneBtn.addEventListener('click', () => {
+    const tasks = getTasks();
+    const task = tasks.find(task => task.id === id);
+    task.checked = !task.checked;
+
+    renderToast('¡Task completed!', 'green');
+    editTask(task);
+    TasksList();
     dialog.close();
     dialog.remove();
   });
