59a43297c76c7994af2c6d0537382de356d08032
Carlos <carlosaac232001@gmail.com>
2025-10-01 17:40:24 -0500

    feat: add validation to prevent crash

--- src/components/TaskDialog/TaskDialog.js
+++ src/components/TaskDialog/TaskDialog.js
@@ -2,6 +2,10 @@ import TaskDialogView from './TaskDialogView';
 import TaskDialogController from './TaskDialogController';
 
 export default function TaskDialog(task) {
+  if (!task) {
+    return console.error('There is no task data');
+  }
+
   const dialog = TaskDialogView(task);
   document.body.appendChild(dialog);
   dialog.showModal();
